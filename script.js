3708a54f099bec0e79011cf99bfafc90293bd949
Damian Swarts <damianswarts2019@gmail.com>
2024-02-07 19:18:24 +0200

    Create a new function playGame() that uses the playRound() function inside it to play a five round game.

--- script.js
+++ script.js
@@ -12,28 +12,57 @@ The function takes two parameters - the playerSelection and computerSelection -
 and then returns a string that declares the winner or tie of the round. */
 
 function playRound(playerSelection, computerSelection) {   
-    var result;    
-    if (
-        playerSelection === "rock" && computerSelection === "Scissors" ||
-        playerSelection === "paper" && computerSelection === "Rock" || 
-        playerSelection === "scissors" && computerSelection === "Paper")
+    var result; 
+    if (playerSelection === computerSelection) 
     {
-        result = `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}.`;
-        return result;     
+        result = "tie";
+        return result;
     } else if (
         playerSelection === "rock" && computerSelection === "Paper" ||
         playerSelection === "paper" && computerSelection === "Scissors" || 
         playerSelection === "scissors" && computerSelection === "Rock")
     {
-        result = `You lose! ${computerSelection} beats ${playerSelection[0].toUpperCase() + playerSelection.substring(1)}.`;
+        result = "computer won";
         return result;
-    } else {
-        result = "This round ended in a draw."
-        return result;
-    }   
+    } else if (
+        playerSelection === "rock" && computerSelection === "Scissors" ||
+        playerSelection === "paper" && computerSelection === "Rock" || 
+        playerSelection === "scissors" && computerSelection === "Paper")
+    {
+        result = "player won";
+        return result;     
+    } 
 }
 
-// Call the functions, prompt the user for input and display the results to the console.
-let playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+/* Create a new function playGame() that uses the playRound() function inside it to play a five round game.
+The function keeps score and reports a winner at the end. */
+
+function playGame(rounds) {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < rounds; i++) {
+        const computerChoice = getComputerChoice();
+        let playerChoice = prompt("Choose your weapon: Rock, Paper or Scissors?");
+        
+        let result = playRound(playerChoice, computerChoice);
+        if(result === "player won") {
+            playerScore++;
+            console.log("You won this round!");
+        } else if(result === "computer won") {
+            computerScore++;
+            console.log("Computer won this round!");
+        } else {
+            console.log("Oops! It's a tie!");
+        }
+    }
+
+    if(playerScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer won the game!");
+    } else if (playerScore === computerScore) {
+        console.log("The game ended in a tie!");
+    }
+}
+
+playGame(5);
