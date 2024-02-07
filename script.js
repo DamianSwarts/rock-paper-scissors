23759f55c0f5ef0c1c425b7a32fc0cdc1cb54355
Damian Swarts <damianswarts2019@gmail.com>
2024-02-07 17:39:09 +0200

    Create a function getComputerChoice() that randomly returns either

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Create a function getComputerChoice() that randomly returns either
+"Rock", "Paper" or "Scissors". The random string returned will be used for the computer's play */
+
+function getComputerChoice(){
+    let myArray = ["Rock", "Paper", "Scissors"];
+    
+    return(myArray[(Math.floor(Math.random() * myArray.length))]);   
+
+}
