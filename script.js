ca6ad35865f3408b9d648b5663e223f4a01bbd74
Damian Swarts <damianswarts2019@gmail.com>
2024-02-26 15:14:55 +0200

    Created a function that plays a round of Rock, Paper, Scissors every time the user presses a button

--- script.js
+++ script.js
@@ -1,68 +1,30 @@
-/* Create a function getComputerChoice() that randomly returns either "Rock", "Paper" or "Scissors". 
-The random string returned will be used for the computer's play */
+const computerChoices = ["rock", "paper", "scissors"];
+const playerResult = document.getElementById("playerResult");
+const computerResult = document.getElementById("computerResult");
+const gameResult = document.getElementById("gameResult");
 
-function getComputerChoice(){
-    let myArray = ["Rock", "Paper", "Scissors"];
-    
-    return(myArray[(Math.floor(Math.random() * myArray.length))]);   
-}
-
-/* Write a function that plays a single round of Rock Paper Scissors. 
-The function takes two parameters - the playerSelection and computerSelection - 
-and then returns a string that declares the winner or tie of the round. */
-
-function playRound(playerSelection, computerSelection) {   
-    var result; 
+// Function that plays a round of Rock, Paper, Scissors every time the user presses a button
+function playGame(playerSelection){
+    // Select a random choice out of an array to use as the computer's play.
+    const computerSelection = computerChoices[Math.floor(Math.random() * 3)]
+    let result ="";
     if (playerSelection === computerSelection) 
     {
-        result = "tie";
-        return result;
-    } else if (
-        playerSelection === "rock" && computerSelection === "Paper" ||
-        playerSelection === "paper" && computerSelection === "Scissors" || 
-        playerSelection === "scissors" && computerSelection === "Rock")
-    {
-        result = "computer won";
-        return result;
-    } else if (
-        playerSelection === "rock" && computerSelection === "Scissors" ||
-        playerSelection === "paper" && computerSelection === "Rock" || 
-        playerSelection === "scissors" && computerSelection === "Paper")
-    {
-        result = "player won";
-        return result;     
-    } 
-}
-
-/* Create a new function playGame() that uses the playRound() function inside it to play a five round game.
-The function keeps score and reports a winner at the end. */
-
-function playGame(rounds) {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < rounds; i++) {
-        const computerChoice = getComputerChoice();
-        let playerChoice = prompt("Choose your weapon: Rock, Paper or Scissors?");
-        
-        let result = playRound(playerChoice, computerChoice);
-        if(result === "player won") {
-            playerScore++;
-            console.log("You won this round!");
-        } else if(result === "computer won") {
-            computerScore++;
-            console.log("Computer won this round!");
-        } else {
-            console.log("Oops! It's a tie!");
-        }
+        result = "IT'S A TIE!";
+    } else {
+        switch (playerSelection) {
+            case "rock":
+                result = (computerSelection === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerSelection === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerSelection === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            }
     }
-
-    if(playerScore > computerScore) {
-        console.log("You won the game!");
-    } else if (computerScore > playerScore) {
-        console.log("Computer won the game!");
-    } else if (playerScore === computerScore) {
-        console.log("The game ended in a tie!");
-    }
+    playerResult.textContent = `PLAYER:  ${playerSelection}`;
+    computerResult.textContent = `COMPUTER:  ${computerSelection}`;
+    gameResult.textContent = result;
 }
-
-playGame(5);
